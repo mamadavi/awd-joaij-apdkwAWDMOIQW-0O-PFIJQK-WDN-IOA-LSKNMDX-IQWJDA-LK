87fff2d7729f0561a4e2b9a5932ee8e51d857b71
mamadavi <sove859@gmail.com>
2024-07-15 11:31:37 +0300

    Update SAKURA.py

--- SAKURA.py
+++ SAKURA.py
@@ -1,22 +1,17 @@
 import subprocess
 import os
-
-# Установка зависимостей (только если их нет)
-try:
-  os.system("sudo apt install nmap nuclei assetfinder subfinder finalrecon lolcat")
-  os.system("pip3 install colorama")
-except:
-  pass
-
 from colorama import Fore, Style, init
 
 
+try:
+  os.system("sudo apt install nmap nuclei assetfinder subfinder finalrecon lolcat")
+except:
+  pass
 
-# Очистка консоли
+
 os.system("printf '\033c'")
 
-# Логотип
-init(autoreset=True)  # Автоматический сброс цвета после каждой строки
+init(autoreset=True)  
 
 print(Fore.LIGHTMAGENTA_EX + "   /$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$ /$$$$$$$   /$$$$$$  ")
 print(Fore.LIGHTMAGENTA_EX + "  /$$__  $$ /$$__  $$| $$  /$$/| $$  | $$| $$__  $$ /$$__  $$ ")
@@ -26,6 +21,7 @@ print(Fore.LIGHTMAGENTA_EX + "  \____  $$| $$__  $$| $$  $$  | $$  | $$| $$__  $
 print(Fore.LIGHTMAGENTA_EX + "  /$$  \ $$| $$  | $$| $$\  $$ | $$  | $$| $$  \ $$| $$  | $$ ")
 print(Fore.LIGHTMAGENTA_EX + " |  $$$$$$/| $$  | $$| $$ \  $$|/ $$$$$$/| $$  | $$| $$  | $$ ")
 print(Fore.LIGHTMAGENTA_EX + "  \______/ |/  |/|/  \/ \______/     |/  |/  |/  |/    |/     ")
+print(Fore.LIGHTMAGENTA_EX + "                                [ Maden by Akiyama and Wigil ]")
 print(Style.RESET_ALL)
 
 while True:
@@ -37,7 +33,6 @@ while True:
   choice = input("Make a choice: ")
 
   if choice == '1':
-    # Меню Пентестинга
     while True:
       print(Fore.CYAN + "Pentesting Menu:")
       print(Fore.CYAN + "1. Start  2. Back to Menu ")
@@ -46,7 +41,6 @@ while True:
       pentest_choice = input("Make a choice: ")
 
       if pentest_choice == '1':
-        # Запускаем пентестинг
         print(Fore.CYAN + "Maden by WIGIL and Akiyama")
         print(Style.RESET_ALL)
 
@@ -64,12 +58,12 @@ while True:
         os.system(f"finalrecon -pt {threads} -dt {dir_threads} --full https://{target}")
         os.system(f"searchsploit --nmap {nmap_result}")
 
-        break # Выходим из меню пентестинга
+        break
 
       elif pentest_choice == '2':
         print(Fore.CYAN + "Back to Menu")
         print(Style.RESET_ALL)
-        break # Возвращаемся в главное меню
+        break
 
       else:
         print(Fore.RED + "Invalid Choice.")
@@ -85,54 +79,82 @@ while True:
       doxing_choice = input("Make a choice: ")
 
       if doxing_choice == '1':
+          
         # Код для запуска доксинга
         # ...
         # Дополнительные действия
         # ...
-        break # Выходим из меню доксинга
+        break
 
       elif doxing_choice == '2':
         print(Fore.CYAN + "Back to Menu")
         print(Style.RESET_ALL)
-        break # Возвращаемся в главное меню
+        break
 
       else:
         print(Fore.RED + "Invalid Choice")
         print(Style.RESET_ALL)
 
   elif choice == '3':
-    # Меню DDoS
-    while True:
-      print(Fore.CYAN + "DDoS Menu:")
-      print(Fore.CYAN + "1. Start  2. Back to Menu")
-      print(Style.RESET_ALL)
+    def show_attack_options():
+        print("Choose the method of attack:")
+        print("1. Karma-DDoS")
+        print("2. Hammer DDoS")
+        print("3. Botnet")
 
-      ddos_choice = input("Make a choice: ")
+        attack_choice = input("Make a choice: ")
 
-      if ddos_choice == '1':
-        # Код для запуска DDoS
-        # ...
+        if attack_choice == "1":
+            print("Karma-DDoS Attack")
+            
+        # Запускаем Karma-DDoS атаку
+        # ... (ваш код для Karma-DDoS)
         # Дополнительные действия
         # ...
-        break # Выходим из меню DDoS
+        
+        elif attack_choice == "2":
+            print("Hammer-DDoS Attack")
+            
+        # Запускаем Hammer DDoS атаку
+        # ... (ваш код для Hammer DDoS)
+        # Дополнительные действия
+        # ...
+        
+        elif attack_choice == "3":
+            print("Botnet Attack")
+            
+        # Запускаем Botnet атаку
+        # ... (ваш код для Botnet)
+        # Дополнительные действия
+        # ...
+        
+        else:
+            print("Invalid Choice")
 
-      elif ddos_choice == '2':
-        print(Fore.CYAN + "Back to Menu")
-        print(Style.RESET_ALL)
-        break # Возвращаемся в главное меню
+    while True:
+        print("Menu:")
+        print("1. Choose the method of attack")
+        print("2. Back To Menu")
 
-      else:
-        print(Fore.RED + "Invalid Choice")
-        print(Style.RESET_ALL)
+        choice = input("Make a choice: ")
+
+        if choice == "1":
+            show_attack_options()
+        elif choice == "2":
+            break
+        else:
+            print("Invalid Choice")
 
   elif choice == '4':
     print(Fore.CYAN + "Exit")
     print(Style.RESET_ALL)
-    break # Выходим из программы
+    break
 
   else:
     print(Fore.RED + "Invalid Choice")
     print(Style.RESET_ALL)
 
+print(Fore.CYAN + "Bye!")
+print(Style.RESET_ALL)
 print(Fore.CYAN + "Bye!")
 print(Style.RESET_ALL)
